9c004af6d685c9a6454d1de248815408db1acaa9
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-06-12 23:27:47 -0300

    creacion de proyecto huerta

---
+++ app/app.js
@@ -0,0 +1,38 @@
+gsap.to('#bg', {
+    scrollTrigger: {
+        scrub:1
+    },
+    scale:1.5
+})
+
+gsap.to('#logo', {
+    scrollTrigger: {
+        scrub:1
+    },
+    y:250,
+    scale:0.8
+});
+
+gsap.to('#nube1', {
+    scrollTrigger: {
+        scrub:1
+    },
+    x:200 
+    
+});
+
+gsap.to('#nube2', {
+    scrollTrigger: {
+        scrub:1
+    },
+    x:-200 
+    
+});
+
+gsap.to('#parrafo', {
+    scrollTrigger: {
+        scrub:1
+    },
+    y:-500 
+    
+});
